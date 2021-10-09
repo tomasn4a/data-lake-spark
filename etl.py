c2b31110be657a16c2d65a189e2123fded39ef92
Tomas Rojo <tomasn4a@gmail.com>
2021-10-09 17:37:52 +0200

    add timestamp table processing

--- etl.py
+++ etl.py
@@ -40,19 +40,7 @@ def create_spark_session() -> SparkSession:
 def process_song_data(spark: SparkSession,
                       input_data: Union[str, Path],
                       output_data: Union[str, Path]) -> None:
-    """
-    Read song data from S3 and write songs and artist tables as parquet.
-
-    Parameters
-    ----------
-    spark : SparkSession
-        A spark session
-    input_data : PathLike
-        A path to the S3 bucket
-    output_data : PathLike
-        The output directory for the parquet files
-
-    """
+    """Read song data from S3 and write songs and artist tables as parquet."""
 
     # Get filepath to song data file
     song_data_path = Path(input_data) / 'song_data'
@@ -74,14 +62,14 @@ def process_song_data(spark: SparkSession,
         .parquet(Path(output_data))
 
     # Extract columns to create artists table
-    artists_fields_exprs = [
+    artists_exprs = [
         'artist_id AS artist_id',
         'artist_name AS name',
         'artist_location AS location',
         'artist_latitude AS latitude',
         'artist_longitude AS longitude'
     ]
-    artists_table = df.selectExpr(*artists_fields_exprs)
+    artists_table = df.selectExpr(*artists_exprs)
 
     # Write artists table to parquet files
     artists_table.write \
@@ -92,27 +80,29 @@ def process_song_data(spark: SparkSession,
 def process_log_data(spark: SparkSession,
                      input_data: Union[str, Path],
                      output_data: Union[str, Path]) -> None:
+    """Read log data from S3 and write users, time, and songplays as parquet."""
+
     # Get filepath to log data file
-    log_data = Path(input_data) / 'log_data'
+    log_data_path = Path(input_data) / 'log_data'
 
     # Read log data files
     df = spark \
         .read \
         .option("recursiveFileLookup", "true") \
-        .json(log_data, schema=log_schema)
+        .json(log_data_path, schema=log_schema)
 
     # Filter by actions for song plays
     df = df.filter(df.page == 'NextSong')
 
     # Extract columns for users table
-    users_fields_exprs = [
+    users_exprs = [
         'userId AS user_id',
         'firstName AS first_name',
         'lastName AS last_name',
         'gender',
         'level'
     ]
-    users_table = df.selectExpr(*users_fields_exprs).limit(5).toPandas()
+    users_table = df.selectExpr(*users_exprs).limit(5).toPandas()
 
     # Write users table to parquet files
     users_table.write \
@@ -122,18 +112,15 @@ def process_log_data(spark: SparkSession,
     # Create timestamp from original timestamp (in ms) column
     df = df.withColumn('ts_timestamp', F.to_timestamp(df.ts/1000))
 
-    # Create date column from new timestamp
-    df = df.withColumn('ts_date', F.to_date(df.ts_timestamp))
-
-    # extract columns to create time table
+    # Extract columns to create time table
     time_exprs = [
         'ts_timestamp AS start_time',
         'HOUR(ts_timestamp) AS hour',
-        'DAY(ts_date) AS day',
-        'WEEKOFYEAR(ts_date) AS week',
-        'MONTH(ts_date) AS month',
-        'YEAR(ts_date) AS year',
-        'WEEKDAY(ts_date) AS weekday'
+        'DAY(ts_timestamp) AS day',
+        'WEEKOFYEAR(ts_timestamp) AS week',
+        'MONTH(ts_timestamp) AS month',
+        'YEAR(ts_timestamp) AS year',
+        'WEEKDAY(ts_timestamp) AS weekday'
     ]
     time_table = df.selectExpr(*time_exprs).limit(5).toPandas()
 
@@ -143,14 +130,39 @@ def process_log_data(spark: SparkSession,
         .mode('overwrite') \
         .parquet(Path(output_data))
 
-    # read in song data to use for songplays table
-    song_df =
+    # Read in song data to use for songplays table
+    song_data_path = Path(input_data) / 'song_data'
+    song_df = spark \
+        .read \
+        .option("recursiveFileLookup", "true") \
+        .json(song_data_path, schema=song_schema)
 
-    # extract columns from joined song and log datasets to create songplays table
-    songplays_table =
+    # Extract songplays columns from joined song and log datasets
+    songplays_exprs = [
+        'songplay_id',
+        'ts_timestamp AS start_time',
+        'userId AS user_id',
+        'level',
+        'song_id',
+        'artist_id',
+        'sessionId AS session_id',
+        'location',
+        'userAgent AS user_agent',
+        'MONTH(ts_timestamp) AS month',
+        'YEAR(ts_timestamp) AS year',
+    ]
+    songplays_table = df \
+        .join(song_df, (df.artist==song_df.artist_name) &
+                       (df.song==song_df.title) &
+                       (df.length==song_df.duration)) \
+        .withColumn('songplay_id', F.monotonically_increasing_id()) \
+        .selectExpr(*songplays_exprs)
 
-    # write songplays table to parquet files partitioned by year and month
-    songplays_table
+    # Write songplays table to parquet files partitioned by year and month
+    songplays_table.write \
+        .partitionBy('year', 'month') \
+        .mode('overwrite') \
+        .parquet(Path(output_data))
 
 
 def main():
